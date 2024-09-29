fc034867056b33b99e808ea8d2d6cf6d3a2e27e3
mr. dima <sobakavosne@gmail.com>
2024-09-29 06:39:06 +0200

    fix(all): fix `isNormalized` & fix tests & add failing cases

--- test/BasisSpec.hs
+++ test/BasisSpec.hs
@@ -4,13 +4,15 @@ module BasisSpec
   ( spec
   ) where
 
-import           Basis           (Colour (Blue, Red, Yellow),
-                                  Move (Horizontal, Vertical), amplitude,
-                                  qInteger, qVector)
-import           Data.Bool       (bool)
-import           Test.Hspec      (Spec, describe, it, shouldBe)
-import           Test.QuickCheck (Arbitrary, arbitrary, choose, elements,
-                                  forAll, property)
+import           Basis             (Colour (Blue, Red, Yellow),
+                                    Move (Horizontal, Vertical), amplitude,
+                                    qInteger, qVector)
+import           Control.Exception (evaluate)
+import           Data.Bool         (bool)
+import           Test.Hspec        (Spec, describe, errorCall, it, shouldBe,
+                                    shouldThrow)
+import           Test.QuickCheck   (Arbitrary, arbitrary, choose, elements,
+                                    forAll, property)
 
 instance Arbitrary Move where
   arbitrary = elements [Vertical, Horizontal]
@@ -21,39 +23,43 @@ instance Arbitrary Colour where
 spec :: Spec
 spec = do
   describe "Quantum Vector Construction (qVector)" $ do
-    it "constructs a quantum vector for Bool type" $
+    it "construct a quantum vector for the Bool type" $
       property $ \b -> do
         let vector = qVector [(False, 1), (True, 0)]
         amplitude vector b `shouldBe` bool 1 0 b
-    it "constructs a quantum vector for Move type" $
+    it "construct a quantum vector for the Move type" $
       property $ \m -> do
         let vector = qVector [(Vertical, 0), (Horizontal, 1)]
         amplitude vector m `shouldBe` bool 0 1 (m == Horizontal)
-    it "constructs a quantum vector for Colour type" $
+    it "construct a quantum vector for the Colour type" $
       property $ \c -> do
         let vector = qVector [(Red, 0.5), (Yellow, 0.7071067812), (Blue, 0.5)]
         amplitude vector c `shouldBe` bool 0.5 0.7071067812 (c == Yellow)
-    it "constructs a quantum vector for Integer type (qInteger)" $
+    it "construct a quantum vector for the Integer type (qInteger)" $
       forAll (choose (100, 1000)) $ \n -> do
         let vector = qInteger
         amplitude vector n `shouldBe` (1 / sqrt (2 ^ n))
+    it
+      "should throw an error while constructing not a normalized quantum vector for the Colour type" $ do
+      evaluate (qVector [(False, 0.6), (True, 0.4)]) `shouldThrow`
+        errorCall "The quantum vector is not normalized."
   describe "Infinite Quantum Vector (qInteger)" $ do
-    it "retrieves amplitude for specific positive integers from qInteger" $
+    it "retrieve amplitude for a specific positive integers from qInteger" $
       forAll (choose (100, 1000)) $ \n -> do
         amplitude qInteger n `shouldBe` (1 / sqrt (2 ^ n))
   describe "Probability Retrieval (amplitude)" $ do
-    it "retrieves probability amplitude for Bool type" $
+    it "retrieve probability amplitude for the Bool type" $
       property $ \b -> do
         let vector = qVector [(False, 1), (True, 0)]
         amplitude vector b `shouldBe` bool 1 0 b
-    it "retrieves probability amplitude for Move type" $
+    it "retrieve probability amplitude for the Move type" $
       property $ \m -> do
         let vector = qVector [(Vertical, 0), (Horizontal, 1)]
         amplitude vector m `shouldBe` bool 0 1 (m == Horizontal)
-    it "retrieves probability amplitude for Colour type" $
+    it "retrieve probability amplitude for the Colour type" $
       property $ \c -> do
-        let vector = qVector [(Red, 0.25), (Yellow, 0.7071067812), (Blue, 0.25)]
-        amplitude vector c `shouldBe` bool 0.25 0.7071067812 (c == Yellow)
-    it "retrieves probability amplitude for Integer type (qInteger)" $
+        let vector = qVector [(Red, 0.5), (Yellow, 1 / sqrt 2), (Blue, 0.5)]
+        amplitude vector c `shouldBe` bool 0.5 (1 / sqrt 2) (c == Yellow)
+    it "retrieve probability amplitude for the Integer type (qInteger)" $
       forAll (choose (100, 1000)) $ \n -> do
         amplitude qInteger n `shouldBe` (1 / sqrt (2 ^ n))
